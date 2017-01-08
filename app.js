9f02eb45285e7ae94cc2a6733d179f5bf0e67807
DESKTOP-LAR3HQ3\mcm79 <smilemcm@gmail.com>
2017-01-08 16:57:31 +0900

    ok

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express')
-
+var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/',function(req,res){
-    res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname,'/public')));
+
+// app.get('/',function(req,res){
+//     res.send('kello World!');
+// });
 
 app.listen(3000,function(){
     console.log('Server On!');
